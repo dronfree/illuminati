52a7797355febada72e46410e10ea525eaf82a02
dronfree <dronfree@gmail.com>
2016-05-21 15:11:58 +0300

    added debug info

--- transmitter/transmitter.ino
+++ transmitter/transmitter.ino
@@ -1,6 +1,8 @@
 #include <VirtualWire.h>
 
 void setup() {
+  Serial.begin(9600);
+  Serial.println("setup");
   vw_set_tx_pin(12);
   vw_setup(2000);// скорость передачи данных в Kbps
 }
@@ -11,12 +13,14 @@ void loop() {
   vw_send((uint8_t *)controller, 1);
   vw_wait_tx();
   digitalWrite(13,0);
-  delay(2000);
+  Serial.println("Sent: 1");
+  delay(700);
 
   controller[0] = '0';
   digitalWrite(13,1);
   vw_send((uint8_t *)controller, 1);
   vw_wait_tx();
   digitalWrite(13,0);
-  delay(2000);
+  Serial.println("Sent: 0");
+  delay(700);
 }
